6df75eb0b27c825dee3a35049617d5abe66f63d0
Universal99OS <pandukapiushanpullaperuma4321@gmail.com>
2024-02-04 20:25:48 +0530

    create book repo

--- src/main/java/org/example/repository/BookRepository.java
+++ src/main/java/org/example/repository/BookRepository.java
@@ -3,5 +3,5 @@ package org.example.repository;
 import org.example.entity.Book;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepository extends CrudRepository<Book,> {
+public interface BookRepository extends CrudRepository<Book,Long> {
 }
